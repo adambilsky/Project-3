670f34b507f9e8699d2d01f63e37a85061ab23ca
Adam Bilsky <adambilsky@gmail.com>
2018-10-10 12:50:08 -0500

    basic MERN-ready restructuring

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,5 @@
+export * from "./Input";
+export * from "./TextArea";
+export * from "./FormBtn";
+
+// Replace with all fields used to compose profiles and/or searches
---
+++ client/src/utils/API.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default {
+// API CRUD functions here
+};
