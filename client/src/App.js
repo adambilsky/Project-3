77a77987a3b084b989ce38c45b4f93aad5990b4d
Will <williamjamesthompson@gmail.com>
2018-10-22 11:48:13 -0500

    App now loads expecting react components. Cleaned up front-end file structures

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component }  from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 // import pages and components here // 
 
 
-
-const App = () => (
-/*  for now, the app is structured like the book app, 
-    but we will add and change as we go */
-
-    <Router>
+class App extends Component {
+    render() {
+        return (
+        <Router>
         <div>
             <Nav />
-            <Switch>
-                <Route exact path="/" component={/*whatever*/} />
+                <Route exact path="/" component={Jumbotron} />
                 {
                 /*  <Route exact path="/" component={} />
                     <Route exact path="/" component={} />
                     <Route exact path="/" component={} /> */
                 }
-                <Route component={NoMatch} />
-            </Switch>
+                <Route />
         </div>
     </Router>
-);
+        );
+    }
+}
+
+
+// const App = () => (
+// /*  for now, the app is structured like the book app, 
+//     but we will add and change as we go */
+
+//     <Router>
+//         <div>
+//             <Nav />
+//                 <Route exact path="/" component={Form} />
+//                 {
+//                 /*  <Route exact path="/" component={} />
+//                     <Route exact path="/" component={} />
+//                     <Route exact path="/" component={} /> */
+//                 }
+//                 <Route component={NoMatch} />
+//         </div>
+//     </Router>
+// );
 
 export default App;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -12,7 +12,7 @@ import React from "react";
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <a className="navbar-brand" href="/">
-      Student Film Database
+      Welcome to IMDBu
     </a>
   </nav>
 );
