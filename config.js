9032c9e8bf848454f9b5023b642db2f0085e1bf5
Julie Ores <julie.ores@gmail.com>
2018-11-08 15:23:51 -0600

    tried remove errors

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+// host: 0.0;
+// port: 3001
+
+
 module.exports = {
 
     'secret': 'iloveproject3',
--- server.js
+++ server.js
@@ -74,9 +74,9 @@ if (token) {
 
 }
 ;
-app.get('/', function(req, res) {
-res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
+// app.get('/', function(req, res) {
+// res.send('Hello! The API is at http://localhost:' + port + '/api')
+// });
 
 // API ROUTES -------------------
 // we'll get to these in a second
