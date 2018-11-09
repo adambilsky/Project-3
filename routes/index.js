b0a427752e024b867bb3e4b867700bff7c811eb9
Will <williamjamesthompson@gmail.com>
2018-11-08 20:05:41 -0600

    heroku

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-router.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 // Add routes, both API and view
 app.use(routes);
