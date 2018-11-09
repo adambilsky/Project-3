4d8056e02ec9360d8a0864648a1fd9502249fc4a
Will <williamjamesthompson@gmail.com>
2018-11-08 19:58:19 -0600

    heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
+app.get('/', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 module.exports = router;
